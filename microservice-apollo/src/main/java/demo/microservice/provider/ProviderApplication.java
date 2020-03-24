2d87d81cc8ea59e666598e4d8b28762391f22308
chengxuhai <2300804096@qq.com>
2020-03-24 18:35:32 +0800

    使用apollo作为配置中心

---
+++ microservice-apollo/src/main/java/demo/microservice/provider/ProviderApplication.java
@@ -0,0 +1,23 @@
+package demo.microservice.provider;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("demo.microservice.provider.dao")
+@EnableApolloConfig
+public class ProviderApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class);
+    }
+
+    public void run(String... args) throws Exception {
+        System.out.println("provider 启动成功");
+    }
+}
